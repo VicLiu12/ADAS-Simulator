8e406be058025e9a9b1a69e7a8a0c770f89ad528
Vic Liu <144785904+VicLiu12@users.noreply.github.com>
2026-05-23 17:41:22 +0800

    Update IMP.py

--- Python/IPM.py
+++ Python/IPM.py
@@ -26,6 +26,53 @@ print('q --> quit')
 
 homography_matrix = None
 
+while True:
+    ret, frame = cap.read()
+    if not ret: break
 
+    frame_undistorted = cv2.undistort(frame, mtx, dist, None, mtx)
+    gray = cv2.cvtColor(frame_undistorted, cv2.COLOR_BGR2GRAY)
+
+    corners, ids, rejected = detector.detectMarkers(gray)
+
+    if ids is not None and len(ids) > 0:
+        cv2.aruco.drawDetectedMarkers(frame_undistorted, corners, ids)
+
+        pts_src = np.array(corners[0][0], dtype="float32")
+
+        offset_x = (BEV_Width - Markers_Pixels) // 2
+        offset_y = BEV_Height -Markers_Pixels -50
+
+        pts_dst = np.array([
+            #左上
+            [offset_x, offset_y],
+            #右上
+            [offset_x + Markers_Pixels, offset_y],
+            #右下
+            [offset_x + Markers_Pixels, offset_y + Markers_Pixels],
+            #左下
+            [offset_x, offset_y + Markers_Pixels]
+        ], dtype="float32")
+
+
+        homography_matrix = cv2.getPerspectiveTransform(pts_src, pts_dst)
+    
+    cv2.imshow("Front View", frame_undistorted)
+
+    if homography_matrix is not None:
+        bev = cv2.warpPerspective(frame_undistorted, homography_matrix, (BEV_Width, BEV_Height))
+        cv2.imshow("Bird EYE", bev)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('s') and homography_matrix is not None:
+        save_path = os.path.join(current_path, 'ipm.npz')
+        np.savez(save_path, M=homography_matrix)
+        print('Save ipm matrix')
+    elif key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
 
