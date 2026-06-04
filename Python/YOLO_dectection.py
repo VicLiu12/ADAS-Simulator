16b1298b20c5737cc9b984ea5e667a64ade6e10a
Vic Liu <144785904+VicLiu12@users.noreply.github.com>
2026-06-04 19:48:21 +0800

    program update

--- Python/YOLO_dectection.py
+++ Python/YOLO_dectection.py
@@ -28,9 +28,55 @@ Camera_BEV_Y = BEV_Height
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-model = YOLO('yolo26n.pt')
+model = YOLO('yolo26m-sem.pt')
 
 while True:
     ret, frame = cap.read()
+    if not ret: 
+        break
+
+    frame_undistorted = cv2.undistort(frame, mtx, dist, None, mtx)
+
+    results = model(frame_undistorted, stream = True, conf= 0.5, verbose = False)
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)
+
+            cls_name = model.names[int(box.cls[0])]
+
+            bottom_center_x = (x1 + x2) / 2
+            bottom_center_y = y2
+
+            pt_src = np.array([[[bottom_center_x, bottom_center_y]]], dtype = np.float32)
+
+            pt_bev = cv2.perspectiveTransform(pt_src, ipm_matrix)
+            bev_x, bev_y = pt_bev[0][0]
+
+            distance_pixels = math.sqrt((bev_x - Camera_BEV_X)**2 + (bev_y - Camera_BEV_Y)**2)
+            distance_meters = distance_pixels * Pixel_To_Meter
+
+            if bev_y > Camera_BEV_Y: 
+                continue
+
+            cv2.rectangle(frame_undistorted, (x1, y1), (x2, y2), (0, 255, 255), 2)
+            cv2.circle(frame_undistorted, (int(bottom_center_x), int(bottom_center_y)), 5, (0, 0, 255), -1)
+
+            text = f"{cls_name} {distance_meters:.2f}m"
+            cv2.putText(frame_undistorted, text, (x1, y1, -10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+            #測試看看會不會有反應
+            if distance_meters < 0.5:
+                print("TOO CLOSE !!!!!!!!!")
+
+    cv2.imshow("YOLO distance detection", frame_undistorted)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
 
 
